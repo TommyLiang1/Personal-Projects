d3058488f5c9b33716e380035c108830d56ee510
TommyLiang1 <tommyliang131@gmail.com>
2023-10-25 21:35:42 -0400

    Update files for gh pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as HashRouter, Route, Routes } from "react-router-dom";
 import RecipeApp from "./components/RecipeApp";
 import Favorite from "./components/Favorite";
 import Recipe from "./components/Recipe";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<RecipeApp />} />
         <Route path="/favorite" element={<Favorite />} />
         <Route path="/recipe/:id" element={<Recipe />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
